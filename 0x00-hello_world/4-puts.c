d6587e05b97ea39c0e9cece2e01c99df8c306fd6
KamauDev-maker <oscarnjenga@gmail.com>
2022-09-08 05:34:37 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
-*Description: main - prints out a string to stdout
-*Returns: 0 if its true
-*/
+ * Description: main - Prints out a string to stdout.
+ * Return: 0 if true.
+ */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
