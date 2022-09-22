5ab5098cdebe072dcc9f9481d988f4db06c17045
KamauDev-maker <oscarnjenga@gmail.com>
2022-09-21 23:46:36 -0700

    capitilize words

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,29 +1,34 @@
 #include "main.h"
+
 /**
- **cap_string -  capitilizes the first letters in word
- *@s: - string to be capitilezed
- *Return: string
+ * cap_string - Capitalizes all first letter in  words
+ * @s: The string to be capitalized
+ * Return: A pointer to the changed string
  */
 char *cap_string(char *s)
 {
-int i = 0;
-while (s[i])
-{
-while (!(s[i] >= 'a' && s[i] <= 'z'))
-i++;
-if (s[i - 1] == ',' ||
-s[i - 1] == ';' ||
-s[i - 1] == '.' ||
-s[i - 1] == '!' ||
-s[i - 1] == '?' ||
-s[i - 1] == '"' ||
-s[i - 1] == '(' ||
-s[i - 1] == ')' ||
-s[i - 1] == '{' ||
-s[i - 1] == '}' ||
-i == 0)
-s[i] -= 32;
-i++;
-}
-return (s);
+	int i = 0;
+
+	while (s[i])
+	{
+		while (!(s[i] >= 'a' && s[i] <= 'z'))
+			i++;
+
+		if (s[i - 1] == ',' ||
+		    s[i - 1] == ';' ||
+		    s[i - 1] == '.' ||
+		    s[i - 1] == '!' ||
+		    s[i - 1] == '?' ||
+		    s[i - 1] == '"' ||
+		    s[i - 1] == '(' ||
+		    s[i - 1] == ')' ||
+		    s[i - 1] == '{' ||
+		    s[i - 1] == '}' ||
+		    i == 0)
+			s[i] -= 32;
+
+		i++;
+	}
+
+	return (s);
 }
