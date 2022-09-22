b147edeab09c0ee8772f635950b3885e48693b59
KamauDev-maker <oscarnjenga@gmail.com>
2022-09-22 01:38:08 -0700

    intgers

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -5,12 +5,17 @@
  */
 void print_number(int n)
 {
-if (n < 0)
-{
-_putchar('-');
-n = -n;
-}
-if (n / 10)
-print_number(n / 10);
-_putchar('0' + n % 10);
+	if (n < 0)
+	{
+		_putchar('-');
+		if (n < -9)
+			print_number(n / -10);
+		_putchar('0' - n % 10);
+	}
+	else
+	{
+		if (n > 9)
+			print_number(n / 10);
+		_putchar(n % 10 + '0');
+	}
 }
