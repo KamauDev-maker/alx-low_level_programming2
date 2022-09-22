c7fff20dbfad5c973a90b436b4aa947dede693a8
KamauDev-maker <oscarnjenga@gmail.com>
2022-09-22 01:06:05 -0700

    encodes 1337

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -6,29 +6,19 @@
  */
 char *leet(char *s)
 {
-int i;
-for (i = 0; s[i] != '\0'; i++)
-{
-if (s[i] == 'a' || s[i] == 'A')
-{
-s[i] = '4';
-}
-else if (s[i] == 'e' || s[i] == 'E')
-{
-s[i] = '3';
-}
-else if (s[i] == 'o' || s[i] == 'O')
-{
-s[i] = '0';
-}
-else if (s[i] == 't' || s[i] == 'T')
-{
-s[i] = '7';
-}
-else if (s[i] == 'l' || s[i] == 'L')
-{
-s[i] = '1';
-}
-}
-return (s);
+	char *r = s;
+	char a[] = { 'a', 'e', 'o', 't', 'l' };
+	char n[] = { 4, 3, 0, 7, 1 };
+	int i = 0;
+
+	while (*s)
+	{
+		for (i = 0; i < 5; i++)
+		{
+			if (*s == a[i] || *s == a[i] - 32)
+				*s = n[i] + '0';
+		}
+		s++;
+	}
+	return (r);
 }
