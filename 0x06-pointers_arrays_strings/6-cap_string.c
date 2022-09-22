45a379108c552e0454e11e6da85e2b22eed65312
KamauDev-maker <oscarnjenga@gmail.com>
2022-09-22 00:17:24 -0700

    capitilize words

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -2,33 +2,39 @@
 
 /**
  * cap_string - Capitalizes all first letter in  words
- * @s: The string to be capitalized
+ * @str: The string to be capitalized
  * Return: A pointer to the changed string
  */
-char *cap_string(char *s)
+char *cap_string(char *str)
 {
-	int i = 0;
+	int i;
 
-	while (s[i])
+	for (i = 0; str[i] != '\0'; i++)
 	{
-		while (!(s[i] >= 'a' && s[i] <= 'z'))
-			i++;
-
-		if (s[i - 1] == ',' ||
-		    s[i - 1] == ';' ||
-		    s[i - 1] == '.' ||
-		    s[i - 1] == '!' ||
-		    s[i - 1] == '?' ||
-		    s[i - 1] == '"' ||
-		    s[i - 1] == '(' ||
-		    s[i - 1] == ')' ||
-		    s[i - 1] == '{' ||
-		    s[i - 1] == '}' ||
-		    i == 0)
-			s[i] -= 32;
-
-		i++;
+		if (i == 0)
+		{
+			if ((str[i] >= 97 && str[i] <= 122))
+			{
+				str[i] = str[i] - 32;
+				continue;
+			}
+		}
+		else if (str[i] == ' ')
+		{
+			++i;
+			if (str[i] >= 97 && str[i] <= 122)
+			{
+				str[i] = str[i] - 32;
+				continue;
+			}
+		}
+		else
+		{
+			if (str[i] >= 65 && str[i] <= 90)
+			{
+				str[i] = str[i] + 32;
+			}
+		}
 	}
-
-	return (s);
+	return (str);
 }
